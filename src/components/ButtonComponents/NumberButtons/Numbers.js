6495d3f2b846bcd9c864d9f2157410d57385ccf1
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-07-25 23:23:29 -0500

    Attempting to add numbers into app. cannot figure out syntax

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+
+import { numbers } from '../../../data'
 
 //import any components needed
 
@@ -6,11 +8,18 @@ import React from "react";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+
+  const [calcNumbers, setCalcNumbers] = state(0)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+
+       {calcNumbers.map(nums => {
+         
+        })
+      }
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Numbers from '../src/components/ButtonComponents/NumberButtons/Numbers'
+
+function HomeApp() {
+
+    return(
+        <Numbers />
+    )
+}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
