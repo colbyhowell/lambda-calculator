0f379540bc9595f8e60cf99a8ecebcd8a9dbccc4
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-07-26 21:22:23 -0500

    figuring out why I'm getting an error

--- src/index.js
+++ src/index.js
@@ -2,15 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Numbers from '../src/components/ButtonComponents/NumberButtons/Numbers'
-
-function HomeApp() {
-
-    return(
-        <Numbers />
-    )
-}
-
 
 
 
